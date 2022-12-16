cc3f475eb2755dc80d20ec2bc514e0b654ef5b6c
Robin Tapiru <robintapiru0894@gmail.com>
2022-12-16 18:07:50 +0400

    new

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$(".statue").hover(
+  function () {
+    $(this).toggleClass('hover');
+    $('.photos-wrapper img:nth-child(1)').addClass('hover');
+    $('.photos-wrapper img:nth-child(2)').addClass('hover2');
+    $('.photos-wrapper img:nth-child(3)').addClass('hover');
+    $('.photos-wrapper img:nth-child(4)').addClass('hover2');
+    $('.bg-hover').addClass('hover');
+  },
+  function () {
+    $(this).toggleClass('hover');
+    $('.photos-wrapper img:nth-child(1)').removeClass('hover');
+    $('.photos-wrapper img:nth-child(2)').removeClass('hover2');
+    $('.photos-wrapper img:nth-child(3)').removeClass('hover');
+    $('.photos-wrapper img:nth-child(4)').removeClass('hover2');
+    $('.bg-hover').removeClass('hover');
+  }
+);
+
+$('.opening span').click(function(){
+  $('.opening').addClass('active');
+  $('.main').removeClass('active');
+  $('.opening span').fadeOut();
+  myAudio.play();
+
+  setTimeout(function() {
+    $('.opening').hide();
+  }, 1000);
+});
+
+// add plaay audio
+var myAudio = document.getElementById("player");
+var isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? myAudio.pause() : myAudio.play();
+};
