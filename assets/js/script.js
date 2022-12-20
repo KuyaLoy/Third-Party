b34561c405e1fa0b51f88f04c78cfc762f421eff
Robin Tapiru <robintapiru0894@gmail.com>
2022-12-20 17:58:44 +0400

    asd

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,69 @@ element.addEventListener('wheel', (event) => {
     
   });
 });
+
+
+
+
+// $( window ).resize(function() {
+//   if (window.matchMedia("(orientation: portrait)").matches) {
+//     // you're in PORTRAIT mode
+//     //alert('portrait');
+//     $('html').css({
+//       'height':' 100vw;',
+//       '-webkit-transform':'rotate(90deg)',
+//       '-moz-transform':'rotate(90deg)',
+//       '-o-transform':'rotate(90deg)',
+//       '-ms-transform':'rotate(90deg)',
+//       'transform':'rotate(90deg)',
+//     });
+  
+//     $('.please-open-landscape').addClass('active');
+
+//     $('body').css('overflow','hidden');
+//   }
+  
+//   if (window.matchMedia("(orientation: landscape)").matches) {
+//     // you're in LANDSCAPE mode
+//     //alert('landscape');
+//     $('html').css({
+//       '-webkit-transform':'rotate(0deg)',
+//       '-moz-transform':'rotate(0deg)',
+//       '-o-transform':'rotate(0deg)',
+//       '-ms-transform':'rotate(0deg)',
+//       'transform':'rotate(0deg)',
+//     });
+//     $('.please-open-landscape').removeClass('active');
+//   }
+// });
+// if (window.matchMedia("(orientation: portrait)").matches) {
+//   // you're in PORTRAIT mode
+//   //alert('portrait');
+//   $('body').css({
+//     'height':' 100vw;',
+//     '-webkit-transform':'rotate(90deg)',
+//     '-moz-transform':'rotate(90deg)',
+//     '-o-transform':'rotate(90deg)',
+//     '-ms-transform':'rotate(90deg)',
+//     'transform':'rotate(90deg)',
+//   });
+
+//   $('.please-open-landscape').addClass('active');
+//   // $('main').hide();
+//   // $('.opening').hide();
+// }
+
+// if (window.matchMedia("(orientation: landscape)").matches) {
+//   // you're in LANDSCAPE mode
+//   //alert('landscape');
+//   $('body').css({
+//     '-webkit-transform':'rotate(0deg)',
+//     '-moz-transform':'rotate(0deg)',
+//     '-o-transform':'rotate(0deg)',
+//     '-ms-transform':'rotate(0deg)',
+//     'transform':'rotate(0deg)',
+//   });
+//   $('.please-open-landscape').removeClass('active');
+//   // $('main').show();
+  
+// }
